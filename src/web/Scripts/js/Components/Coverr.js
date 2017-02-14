1ea4a03fbf8e7ee47314ccb7273049b1102c20ac
Steve Celius <steve@celius.cc>
2017-02-14 01:01:46 +0100

    Could not click buttons (add to cart etc)

--- src/web/Scripts/js/Components/Coverr.js
+++ src/web/Scripts/js/Components/Coverr.js
@@ -68,10 +68,19 @@ $(document).ready(function () {
         var target = this.hash;
         var $target = $(target);
 
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 900, 'swing', function () {
-            window.location.hash = target;
-        });
+        if ($target.offset()) {
+            $('html, body')
+                .stop()
+                .animate(
+                    {
+                        'scrollTop': $target.offset().top
+                    },
+                    900,
+                    'swing',
+                    function() {
+                        window.location.hash = target;
+                    });
+        }
+
     });
 });
