18e019577d0706e21884fed59be0e1ec96382b19
Steve Celius <steve@celius.cc>
2017-02-15 19:18:07 +0100

    Added support for Zendesk chat, with order info and chat link on order history.

---
+++ src/web/Business/ClientResources/ZopimChatClientResourceRegister.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using EPiServer.Framework.Web.Resources;
+using EPiServer.Security;
+using EPiServer.ServiceLocation;
+using Mediachase.Commerce.Customers;
+using Mediachase.Commerce.Security;
+
+namespace OxxCommerceStarterKit.Web.Business.ClientResources
+{
+    [ClientResourceRegister]
+
+    public class ZopimChatClientResourceRegister : IClientResourceRegister
+    {
+        public void RegisterResources(IRequiredClientResourceList requiredResources, HttpContextBase context)
+        {
+            IPrincipal currentPrincipal = PrincipalInfo.CurrentPrincipal;
+            if (currentPrincipal != null && currentPrincipal.Identity.IsAuthenticated)
+            {
+                var contact = CustomerContext.Current.CurrentContact;
+                if (contact != null)
+                {
+                    string script = @"
+   $zopim(function(){
+           $zopim.livechat.setEmail('" + contact.Email + @"');
+           $zopim.livechat.setName('" + contact.FirstName + " " + contact.LastName + @"');
+    });
+";
+                    requiredResources.RequireScriptInline(script).AtFooter();
+                }
+            }
+        }
+    }
+}
