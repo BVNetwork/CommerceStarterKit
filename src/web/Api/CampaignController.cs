492e427e1b85989e0754c076fcfccdfdc8e88ad3
Haakon Peder Haugsten <haakon@episerver.no>
2017-11-19 12:10:35 +0100

    Added Campaign opt-in web-hook.

---
+++ src/web/Api/CampaignController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace OxxCommerceStarterKit.Web.Api
+{
+    public class CampaignController : BaseApiController
+    {
+              
+        [HttpPost]
+        public async Task<string> WebhookSubscribe([FromBody]JToken jsonbody)
+        {
+
+            var dataset = jsonbody.ToObject<EmailResult>();
+
+            if (dataset != null)
+            {
+                if (!string.IsNullOrWhiteSpace(dataset.Email))
+                {
+                    var url = GetUrl(dataset.Email);
+
+                    using (var client = new System.Net.Http.HttpClient())
+                    {
+                        using (var response = await client.GetAsync(new Uri(url)))
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            return content;
+                        }
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private static string GetUrl(string email)
+        {
+            var urlFormat = ConfigurationManager.AppSettings["CampaignSubscribeUrl"];
+            var key = ConfigurationManager.AppSettings["CampaignKey"];
+            var bmOptInId = ConfigurationManager.AppSettings["CampaignOptInId"];
+            var bmOptinSource = ConfigurationManager.AppSettings["CampaignOptInSource"];
+
+            return string.Format(urlFormat, key, bmOptInId, email, bmOptinSource);
+        }
+
+        internal class EmailResult
+        {
+            public string Email { get; set; }
+        }
+    }
+}
