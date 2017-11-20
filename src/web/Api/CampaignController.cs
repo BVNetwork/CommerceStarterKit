ff770b464823aedcf8da407ee2bcbb8895d1e224
Haakon Peder Haugsten <haakon@episerver.no>
2017-11-20 09:29:02 +0100

    Fixed webhook

--- src/web/Api/CampaignController.cs
+++ src/web/Api/CampaignController.cs
@@ -2,16 +2,22 @@
 using System.Configuration;
 using System.Threading.Tasks;
 using System.Web.Http;
+using EPiServer.Logging;
+using EPiServer.ServiceLocation;
 using Newtonsoft.Json.Linq;
 
 namespace OxxCommerceStarterKit.Web.Api
 {
     public class CampaignController : BaseApiController
     {
-              
+
+        private Injected<ILogger> _logger;
+
         [HttpPost]
         public async Task<string> WebhookSubscribe([FromBody]JToken jsonbody)
         {
+            if(_logger.Service.IsInformationEnabled())
+                _logger.Service.Log(Level.Information, "WebhookSubscribe");
 
             var dataset = jsonbody.ToObject<EmailResult>();
 
@@ -25,7 +31,12 @@ namespace OxxCommerceStarterKit.Web.Api
                     {
                         using (var response = await client.GetAsync(new Uri(url)))
                         {
+                            
                             var content = await response.Content.ReadAsStringAsync();
+
+                            if (_logger.Service.IsInformationEnabled())
+                                _logger.Service.Log(Level.Information, "WebhookSubscribe " + content);
+
                             return content;
                         }
                     }
@@ -37,12 +48,12 @@ namespace OxxCommerceStarterKit.Web.Api
 
         private static string GetUrl(string email)
         {
-            var urlFormat = ConfigurationManager.AppSettings["CampaignSubscribeUrl"];
+           // var urlFormat = ConfigurationManager.AppSettings["CampaignSubscribeUrl"];
             var key = ConfigurationManager.AppSettings["CampaignKey"];
             var bmOptInId = ConfigurationManager.AppSettings["CampaignOptInId"];
             var bmOptinSource = ConfigurationManager.AppSettings["CampaignOptInSource"];
 
-            return string.Format(urlFormat, key, bmOptInId, email, bmOptinSource);
+            return string.Format("https://api.broadmail.de/http/form/{0}/subscribe?bmOptInId={1}&bmRecipientId={2}&bmOptinSource={3}", key, bmOptInId, email, bmOptinSource);
         }
 
         internal class EmailResult
