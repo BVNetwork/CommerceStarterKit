c57d7a24c0abebe110b3c5918f66ea24731b32d8
Haugsten <haakon@episerver.no>
2018-01-31 14:29:16 +0100

    Installed Social.Reach and Episerver.Labs.Cognitive

--- src/web/Models/Files/ImageFile.cs
+++ src/web/Models/Files/ImageFile.cs
@@ -10,6 +10,7 @@ Copyright (C) 2013-2014 BV Network AS
 
 using System;
 using System.ComponentModel.DataAnnotations;
+using Episerver.Labs.Cognitive.Attributes;
 //using Episerver.Labs.Cognitive.Attributes;
 using EPiServer;
 using EPiServer.Core;
@@ -55,7 +56,7 @@ namespace OxxCommerceStarterKit.Web.Models.Files
             public const string Large = "?preset=largethumbnail";
         }
 
-    //    public virtual String Description { get; set; }
+        
         public virtual Url Link { get; set; }
         public virtual String Copyright { get; set; }
         public virtual String VideoUrl { get; set; }
@@ -81,45 +82,45 @@ namespace OxxCommerceStarterKit.Web.Models.Files
         public virtual Blob LargeThumbnail { get; set; }
 
         //Creates a descriptive text in english.
-      //  [Vision(VisionType = VisionTypes.Description)]
+        [Vision(VisionType = VisionTypes.Description)]
         [UIHint(UIHint.Textarea)]
         public virtual string Description { get; set; }
 
         //Assigns tags for the image in a comma separated list
-      //  [Vision(VisionType = VisionTypes.Tags, Separator = ",")]
+        [Vision(VisionType = VisionTypes.Tags, Separator = ",")]
         [UIHint(UIHint.Textarea)]
         [Display(Order = 310)]
         public virtual string Tags { get; set; }
 
         //A list of faces identified in the image with their age and gender. It's also possible to just extract ages or gender.
-      //  [Vision(VisionType = VisionTypes.Faces, Separator = ",")]
+        [Vision(VisionType = VisionTypes.Faces, Separator = ",")]
         [UIHint(UIHint.Textarea)]
         [Display(Order = 305)]
         public virtual string Faces { get; set; }
 
 
         //True if the image contains adult content. Useful for moderation
-      //  [Vision(VisionType = VisionTypes.Adult)]
+        [Vision(VisionType = VisionTypes.Adult)]
         [Display(Order = 310)]
         public virtual bool IsAdultContent { get; set; }
 
         //True if the image contains racy content. USeful for moderation
-      //  [Vision(VisionType = VisionTypes.Racy)]
+        [Vision(VisionType = VisionTypes.Racy)]
         [Display(Order = 320)]
         public virtual bool IsRacyContent { get; set; }
 
         //True if the image is clipart
-      //  [Vision(VisionType = VisionTypes.ClipArt)]
+        [Vision(VisionType = VisionTypes.ClipArt)]
         [Display(Order = 330)]
         public virtual bool IsClipArt { get; set; }
 
         //True if the image is a line drawing
-      //  [Vision(VisionType = VisionTypes.LineDrawing)]
+        [Vision(VisionType = VisionTypes.LineDrawing)]
         [Display(Order = 340)]
         public virtual bool IsLineDrawing { get; set; }
 
         //True if the image is black and white
-     //   [Vision(VisionType = VisionTypes.BlackAndWhite)]
+        [Vision(VisionType = VisionTypes.BlackAndWhite)]
         [Display(Order = 350)]
         public virtual bool IsBlackAndWhite { get; set; }
 
@@ -128,22 +129,22 @@ namespace OxxCommerceStarterKit.Web.Models.Files
         //public virtual string AccentColor { get; set; }
 
         //Hex code of the dominant background color
-      //  [Vision(VisionType = VisionTypes.DominantBackgroundColor)]
+        [Vision(VisionType = VisionTypes.DominantBackgroundColor)]
         [Display(Order = 355)]
         public virtual string DominantBackgroundColor { get; set; }
 
         //Hex code of the foreground color
-      //  [Vision(VisionType = VisionTypes.DominantForegroundColor)]
+        [Vision(VisionType = VisionTypes.DominantForegroundColor)]
         [Display(Order = 360)]
         public virtual string DominantForegroundColor { get; set; }
 
 
         //Text recognized in the image
-      //  [Vision(VisionType = VisionTypes.Text)]
+        [Vision(VisionType = VisionTypes.Text)]
         [Display(Order = 370)]
         public virtual string TextRecognized { get; set; }
 
-      //  [Vision(VisionType = VisionTypes.Text)]
+        [Vision(VisionType = VisionTypes.Text)]
         [Display(Order = 380)]
         public virtual XhtmlString TextInPicture { get; set; }
     }
