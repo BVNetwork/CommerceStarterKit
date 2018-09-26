226bce678612e44f03ce40be729173e286acc863
Haugsten <haakon@episerver.no>
2018-09-26 21:29:57 +0200

    Added ProfileStoreService

---
+++ src/web/Business/CustomTracking/ProfileStoreService.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using EPiServer.ServiceLocation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace OxxCommerceStarterKit.Web.Business.CustomTracking
+{
+    public interface IProfileStoreService
+    {        
+        void UpdatePayload(List<string> categories, HttpContextBase context);
+    }
+
+    [ServiceConfiguration(typeof(IProfileStoreService))]
+    public class ProfileStoreService : IProfileStoreService
+    {
+        private readonly HttpClient _client = new HttpClient();
+
+        private readonly string _apiBaseUrl =  ConfigurationManager.AppSettings["ProfileStore.Url"];
+        private readonly string _subscriptionKey = ConfigurationManager.AppSettings["ProfileStore.SubscriptionKey"];
+        private readonly string getProfilesUrl = "api/v1.0/Profiles";
+
+
+        public void UpdatePayload(List<string> categories, HttpContextBase context)
+        {
+            string sessionId = context.Request.Cookies["_madid"]?.Value;
+
+            if (sessionId == null)
+                return;
+
+            var response = Request(HttpMethod.Get, $"{_apiBaseUrl}/{getProfilesUrl}/?$filter=DeviceIds+eq+" + sessionId);
+            
+            var getProfile = response.Content.ReadAsStringAsync();
+                        
+            var profileObject = UpdateData(JObject.Parse(getProfile.Result), categories);
+
+            if (profileObject != null)
+            {
+                var profileId = profileObject["ProfileId"];
+
+                Request(HttpMethod.Put, $"{_apiBaseUrl}/{getProfilesUrl}/{profileId}", profileObject.ToString());
+            }
+        }
+
+      
+        private JObject UpdateData(JObject jToken, List<string> categories)
+        {
+            if (jToken["total"].ToString() != "1")
+                return null;
+            
+            var profile = jToken["items"][0];
+            
+            var originalPayload = JsonConvert.DeserializeObject<Dictionary<string, object>>(profile["Payload"].ToString());
+
+            if (originalPayload == null)
+                originalPayload = new Dictionary<string, object>();
+
+            UpdateOriginalPayload(categories, ref originalPayload);
+
+            profile["Payload"] = JObject.FromObject(originalPayload);
+         
+            return JObject.FromObject(profile);
+        }
+
+
+        private static void UpdateOriginalPayload(IEnumerable<string> categories, ref Dictionary<string, object> originalPayload)
+        {
+
+            if (originalPayload.ContainsKey("ProductsCategories"))
+            {
+                var list = JsonConvert.DeserializeObject<List<string>>(originalPayload["ProductsCategories"].ToString());
+
+                if (list != null)
+                {
+                    list.AddRange(categories);
+                    originalPayload["ProductsCategories"] = list.Distinct();
+                }
+            }
+            else
+            {
+                originalPayload["ProductsCategories"] = categories.Distinct();
+            }
+
+
+            if (originalPayload.ContainsKey("NumberOfOrders"))
+            {
+                var numberOfOrdersString = JsonConvert.DeserializeObject<string>(originalPayload["NumberOfOrders"].ToString());
+                var numberOfOrders = int.Parse(numberOfOrdersString);
+                originalPayload["NumberOfOrders"] = (numberOfOrders + 1).ToString();
+            }
+            else
+            {
+                originalPayload["NumberOfOrders"] = "1";
+            }      
+        }
+
+
+        private HttpResponseMessage Request(HttpMethod pMethod, string pUrl, string pJsonContent = "")
+        {
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = pMethod,
+                RequestUri = new Uri(pUrl)
+            };
+
+            httpRequestMessage.Headers.Add("Authorization", "epi-single" + _subscriptionKey);
+
+            switch (pMethod.Method)
+            {
+                case "POST":
+                case "PUT":
+                    HttpContent httpContent = new StringContent(pJsonContent, Encoding.UTF8, "application/json");
+                    httpRequestMessage.Content = httpContent;
+                    break;
+            }
+
+            var result = _client.SendAsync(httpRequestMessage);
+
+            return result.Result;
+        }
+    }
+}
--- src/web/Business/Recommendations/RecommendationService.cs
+++ src/web/Business/Recommendations/RecommendationService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -5,13 +6,17 @@ using EPiServer;
 using EPiServer.Commerce.Catalog.ContentTypes;
 using EPiServer.Commerce.Order;
 using EPiServer.Core;
+using EPiServer.Find;
+using EPiServer.Find.Framework;
 using EPiServer.Personalization.Commerce.Tracking;
 using EPiServer.ServiceLocation;
 using EPiServer.Tracking.Commerce;
 using EPiServer.Tracking.Commerce.Data;
 using EPiServer.Tracking.Core;
 using OxxCommerceStarterKit.Core.Extensions;
+using OxxCommerceStarterKit.Web.Business.CustomTracking;
 using OxxCommerceStarterKit.Web.EditorDescriptors;
+using OxxCommerceStarterKit.Web.Models.FindModels;
 using OxxCommerceStarterKit.Web.Models.PageTypes;
 
 namespace OxxCommerceStarterKit.Web.Business.Recommendations
@@ -21,14 +26,16 @@ namespace OxxCommerceStarterKit.Web.Business.Recommendations
     {
         private readonly TrackingDataFactory _trackingDataFactory;
         private readonly ITrackingService _trackingService;
+        private readonly IProfileStoreService _profileStoreService;
 
         private readonly HomePage _homePage;
         private readonly RecommendationsMode _mode;
 
-        public RecommendationService(TrackingDataFactory trackingDataFactory, ITrackingService trackingService, IContentLoader contentLoader)
+        public RecommendationService(TrackingDataFactory trackingDataFactory, ITrackingService trackingService, IContentLoader contentLoader, IProfileStoreService profileStoreService)
         {
             _trackingDataFactory = trackingDataFactory;
             _trackingService = trackingService;
+            _profileStoreService = profileStoreService;
 
             _homePage = contentLoader.Get<HomePage>(ContentReference.StartPage);
             _mode = _homePage.Settings.RecommendationsMode.ToEnum(RecommendationsMode.Disabled);
@@ -66,8 +73,58 @@ namespace OxxCommerceStarterKit.Web.Business.Recommendations
 
         public void TrackOrder(IPurchaseOrder purchaseOrder, HttpContextBase context, IContent content)
         {
+            var categories = new List<string>();
             var trackingData = _trackingDataFactory.CreateOrderTrackingData(purchaseOrder, context);
+
+            var items = new List<CartItemData>();
+
+            foreach (var item in trackingData.Order.Items)
+            {
+                var sku = item.Variant?.Sku;
+                
+                var newItem = new CustomCartItemData(item.RefCode, sku, item.Quantity, item.Price);
+
+                var product = GetProduct(item.RefCode);
+                if (product != null)
+                {
+                    newItem.Categories = new List<string>();
+                    foreach (var category in product.ParentCategoryName)
+                    {
+                        var name = category.Replace(" ", "");
+                        var brand = product.Brand?.Replace(" ", "");
+                        newItem.Categories.Add(name);
+                        newItem.Categories.Add(name + "&amp;" + brand);
+                    }
+
+                    categories.AddRange(newItem.Categories);
+                }
+
+                items.Add(newItem);                
+            }
+
+            trackingData = new OrderTrackingData(items, trackingData.Order.Currency,trackingData.Order.Subtotal, trackingData.Order.Shipping, trackingData.Order.Total, trackingData.Order.OrderNo, trackingData.Lang, context, trackingData.User);
+
             _trackingService.Track(trackingData, context, content);
+
+            try
+            {
+                _profileStoreService.UpdatePayload(categories, context);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private FindProduct GetProduct(string itemRefCode)
+        {
+            var result = SearchClient.Instance.Search<FindProduct>()
+                .Filter(x => x.Language.Match("en"))
+                .Filter(x => x.Code.MatchCaseInsensitive(itemRefCode))
+                .GetResult();
+
+            return result.FirstOrDefault();
         }
 
         private IDictionary<string, IEnumerable<Recommendation>> GetRecommendations(CommerceTrackingData trackingData, HttpContextBase context, IContent content)
@@ -104,6 +161,16 @@ namespace OxxCommerceStarterKit.Web.Business.Recommendations
 
             
             return false;
+        }
+    }
+
+    public class CustomCartItemData : CartItemData
+    {
+        public List<string> Categories { get; set; }
+
+        public CustomCartItemData(string refCode, string variantCode, int quantity, Decimal price) : base(refCode,
+            variantCode, quantity, price)
+        {
         }
     }
 }
